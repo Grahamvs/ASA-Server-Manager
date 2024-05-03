20c53ca3a9bd2dea515d6228a5478a689633d4ce
Grahamvs <grahamvs@gmail.com>
2024-05-03 23:13:09 +0100

    Refactored AvailableModsViewModel.

--- ASA Server Manager/ViewModels/AvailableModsViewModel.cs
+++ ASA Server Manager/ViewModels/AvailableModsViewModel.cs
@@ -16,13 +16,10 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
     #region Private Fields
 
     private readonly IApplicationService _applicationService;
-    private readonly ActionCommand _deleteModCommand;
     private readonly IModService _modService;
-    private readonly ActionCommand<MoveDirection> _moveSelectedModCommand;
-    private readonly ActionCommand _saveCommand;
     private ObservableCollection<Mod> _availableModsList;
+    private string _filterText;
     private bool _isBusy;
-    private string _modFilterText;
     private ICollectionView _modListView;
     private Mod _selectedMod;
 
@@ -32,45 +29,75 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
 
     public AvailableModsViewModel(
         IApplicationService applicationService,
-        IModService modService
-    )
+        IModService modService,
+        IDialogService dialogService)
     {
         _applicationService = applicationService;
         _modService = modService;
 
-        _saveCommand = new ActionCommand(ExecuteSaveCommand, CanExecuteSaveCommand);
-        _deleteModCommand = new ActionCommand(ExecuteDeleteModCommand, CanExecuteDeleteModCommand);
-        _moveSelectedModCommand = new ActionCommand<MoveDirection>(ExecuteMoveSelectedModCommand, CanExecuteMoveSelectedModCommand);
+        var baseCommand = new ActionCommand(() => { }, () => !IsBusy)
+            .ObservesProperty(() => IsBusy);
+
+        SaveCommand = CreateBasedCommand(new ActionCommand(ExecuteSaveCommand));
+
+        var selectedItemBaseCommand = new ActionCommand(() => { }, () => SelectedMod != null)
+            .ObservesProperty(() => SelectedMod);
+
+        DeleteCommand = CreateBasedCommand(
+            selectedItemBaseCommand,
+            new ActionCommand(() => _availableModsList.Remove(SelectedMod))
+        );
+
+        MoveSelectedItemCommand = CreateBasedCommand(
+            selectedItemBaseCommand,
+            new ActionCommand<MoveDirection>(ExecuteMoveSelectedModCommand, CanExecuteMoveSelectedModCommand)
+        );
+
+        //// Local Functions \\\\
+        return;
+
+        CompositeCommand CreateBasedCommand(params ICommand[] commands)
+        {
+            var commandList = new List<ICommand> { baseCommand };
+            commandList.AddRange(commands);
+
+            return new(commandList.ToArray())
+            {
+                ErrorHandler = exception => dialogService.ShowErrorMessage(exception.Message),
+                BeginExecuteAction = _ => IsBusy = true,
+                EndExecuteAction = _ => IsBusy = false,
+            };
+        }
     }
 
     #endregion
 
     #region Public Properties
 
-    public ICommand DeleteModCommand => _deleteModCommand;
+    public ICommand DeleteCommand { get; }
+
+    public string FilterText
+    {
+        get => _filterText;
+        set => SetProperty(ref _filterText, value, ModListView.Refresh);
+    }
 
     public bool IsBusy
     {
         get => _isBusy;
-        private set => SetProperty(ref _isBusy, value, OnIsBusyChanged);
-    }
-
-    public string ModFilterText
-    {
-        get => _modFilterText;
-        set => SetProperty(ref _modFilterText, value, ModListView.Refresh);
+        private set => SetProperty(ref _isBusy, value);
     }
 
     public ICollectionView ModListView => _modListView;
 
-    public ICommand MoveSelectedModCommand => _moveSelectedModCommand;
+    public ICommand MoveSelectedItemCommand { get; }
 
-    public ICommand SaveCommand => _saveCommand;
+    public ICommand SaveCommand { get; }
 
     public Mod SelectedMod
     {
         get => _selectedMod;
-        set => SetProperty(ref _selectedMod, value, OnSelectedModChanged);
+        set => SetProperty(ref _selectedMod, value);
     }
 
     public string WindowTitle => $"{_applicationService.ExeName}: Available Mods";
@@ -84,7 +111,7 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
         _availableModsList = new ObservableCollection<Mod>(_modService.AvailableModsList);
 
         var viewSource = CollectionViewSource.GetDefaultView(_availableModsList);
-        viewSource.Filter = OnModsFilter;
+        viewSource.Filter = OnFilter;
         _modListView = viewSource;
 
         RaisePropertyChanged(nameof(ModListView));
@@ -98,15 +125,8 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
 
     #region Private Methods
 
-    private bool CanExecuteDeleteModCommand() => !IsBusy && SelectedMod != null;
-
     private bool CanExecuteMoveSelectedModCommand(MoveDirection direction)
     {
-        if (IsBusy || SelectedMod == null)
-        {
-            return false;
-        }
-
         var index = _availableModsList.IndexOf(SelectedMod);
 
         var canMove = direction switch
@@ -117,13 +137,6 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
         };
 
         return canMove;
-    }
-
-    private bool CanExecuteSaveCommand() => !IsBusy;
-
-    private void ExecuteDeleteModCommand()
-    {
-        _availableModsList.Remove(SelectedMod);
     }
 
     private void ExecuteMoveSelectedModCommand(MoveDirection direction)
@@ -144,62 +157,31 @@ public class AvailableModsViewModel : WindowViewModel, IAvailableModsViewModel
 
         _availableModsList.Move(oldIndex, newIndex);
 
-        _moveSelectedModCommand.RaiseCanExecuteChanged();
+        RaisePropertyChanged(nameof(SelectedMod));
     }
 
     private void ExecuteSaveCommand()
     {
-        if (!CanExecuteSaveCommand())
-        {
-            return;
-        }
+        _modService.SetMods(_availableModsList);
 
-        try
-        {
-            IsBusy = true;
+        _modService.Save();
 
-            _modService.SetMods(_availableModsList);
-
-            _modService.Save();
-
-            RaiseCloseRequested(true);
-        }
-        finally
-        {
-            IsBusy = false;
-        }
+        RaiseCloseRequested(true);
     }
 
-    private void OnIsBusyChanged()
-    {
-        UpdateButtonStates();
-    }
-
-    private bool OnModsFilter(object obj)
+    private bool OnFilter(object obj)
     {
         if (obj is not Mod mod)
             return false;
 
-        if (ModFilterText.IsNullOrEmpty())
+        if (FilterText.IsNullOrEmpty())
             return true;
 
         return CheckValue(mod.ID.ToString())
             || CheckValue(mod.Name)
             || CheckValue(mod.Comments);
 
-        bool CheckValue(string value) => (value ?? string.Empty).Contains(ModFilterText, StringComparison.OrdinalIgnoreCase);
-    }
-
-    private void OnSelectedModChanged()
-    {
-        UpdateButtonStates();
-    }
-
-    private void UpdateButtonStates()
-    {
-        _moveSelectedModCommand.RaiseCanExecuteChanged();
-        _deleteModCommand.RaiseCanExecuteChanged();
-        _saveCommand.RaiseCanExecuteChanged();
+        bool CheckValue(string value) => (value ?? string.Empty).Contains(FilterText, StringComparison.OrdinalIgnoreCase);
     }
 
     #endregion
