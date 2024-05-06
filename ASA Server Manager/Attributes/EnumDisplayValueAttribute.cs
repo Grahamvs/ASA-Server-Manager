11adfe608e17e122deee469c53356d6b6fe674c7
Grahamvs <grahamvs@gmail.com>
2024-05-06 02:26:30 +0100

    Created EnumDisplayValueAttribute and EnumToDisplayValueConverter.

---
+++ ASA Server Manager/Attributes/EnumDisplayValueAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace ASA_Server_Manager.Attributes
+{
+    public class EnumDisplayValueAttribute : Attribute
+    {
+        public string DisplayValue { get; }
+
+        public EnumDisplayValueAttribute(string displayValue)
+        {
+            DisplayValue = displayValue;
+        }
+    }
+}
---
+++ ASA Server Manager/Common/Converters/EnumToDisplayValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+using System.Windows.Markup;
+using ASA_Server_Manager.Attributes;
+
+namespace ASA_Server_Manager.Common.Converters
+{
+    public class EnumToDisplayValueConverter : MarkupExtension, IValueConverter
+    {
+        #region Public Methods
+
+        public static string Convert(Enum value)
+        {
+            if (value == null)
+                return null;
+
+            var attribute = value
+                .GetType()
+                .GetField(value.ToString())
+                ?.GetCustomAttributes(typeof(EnumDisplayValueAttribute), false)
+                .FirstOrDefault() as EnumDisplayValueAttribute;
+
+            // If there is no display value set, attempt to split the text on camel casing.
+            return attribute != null
+                ? attribute.DisplayValue
+                : Regex.Replace(value.ToString(), "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 ");
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => Convert(value as Enum);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        #endregion
+    }
+}
